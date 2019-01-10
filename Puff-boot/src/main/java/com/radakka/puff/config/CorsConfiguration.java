c262e0ce00003491d54e750fe44865bc8784e3e5
jaimegeh <jaimegeh@gmail.com>
2019-01-10 15:48:20 +0100

    Change default port to 8180, allow all cors origins

---
+++ Puff-boot/src/main/java/com/radakka/puff/config/CorsConfiguration.java
@@ -0,0 +1,17 @@
+package com.radakka.puff.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.config.CorsRegistry;
+import org.springframework.web.reactive.config.EnableWebFlux;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
+
+@Configuration
+@EnableWebFlux
+public class CorsConfiguration implements WebFluxConfigurer {
+
+	@Override
+	public void addCorsMappings(CorsRegistry corsRegistry) {
+		corsRegistry.addMapping("/**");
+	}
+	
+}
