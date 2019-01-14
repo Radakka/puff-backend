e9f127704e7b2c8a220dcb849ac48e0ef9cf611d
jaimegeh <jaimegeh@gmail.com>
2019-01-14 13:53:21 +0100

    Changes in security configuration

--- Puff-boot/src/main/java/com/radakka/puff/config/SecurityConfiguration.java
+++ Puff-boot/src/main/java/com/radakka/puff/config/SecurityConfiguration.java
@@ -2,23 +2,23 @@ package com.radakka.puff.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.ReactiveAuthenticationManager;
 import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
-
-import com.radakka.puff.security.AuthenticationManager;
-import com.radakka.puff.security.SecurityContextRepository;
+import org.springframework.security.web.server.context.ServerSecurityContextRepository;
 
 @EnableWebFluxSecurity
 @EnableReactiveMethodSecurity
 public class SecurityConfiguration {
 
 	@Bean
-	public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http, AuthenticationManager authenticationManager, SecurityContextRepository securityContextRepository) {
+	public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http, ReactiveAuthenticationManager authenticationManager, ServerSecurityContextRepository securityContextRepository) {
 		http
 		.csrf().disable()
 		.httpBasic().disable()
+		.logout().disable()
 		.authenticationManager(authenticationManager)
 		.securityContextRepository(securityContextRepository)
 		.authorizeExchange()
