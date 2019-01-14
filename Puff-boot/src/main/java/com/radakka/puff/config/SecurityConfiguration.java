69b0f132005caac4f7d5092b5f86914ab2dbcd7c
jaimegeh <jaimegeh@gmail.com>
2019-01-14 15:04:59 +0100

    Refactor CORS configuration to send headers in 401 unathorized responses

--- Puff-boot/src/main/java/com/radakka/puff/config/SecurityConfiguration.java
+++ Puff-boot/src/main/java/com/radakka/puff/config/SecurityConfiguration.java
@@ -1,13 +1,20 @@
 package com.radakka.puff.config;
 
+import java.util.Arrays;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.ReactiveAuthenticationManager;
 import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.SecurityWebFiltersOrder;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.security.web.server.context.ServerSecurityContextRepository;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.server.WebFilter;
 
 @EnableWebFluxSecurity
 @EnableReactiveMethodSecurity
@@ -16,6 +23,7 @@ public class SecurityConfiguration {
 	@Bean
 	public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http, ReactiveAuthenticationManager authenticationManager, ServerSecurityContextRepository securityContextRepository) {
 		http
+		.addFilterAt(this.corsFilter(), SecurityWebFiltersOrder.FIRST)
 		.csrf().disable()
 		.httpBasic().disable()
 		.logout().disable()
@@ -27,6 +35,17 @@ public class SecurityConfiguration {
 		.anyExchange().authenticated();
 
 		return http.build();
+	}
+	
+	public WebFilter corsFilter() {
+		CorsConfiguration corsConfig = new CorsConfiguration();
+		corsConfig.applyPermitDefaultValues();
+	 
+	    UrlBasedCorsConfigurationSource source =
+	      new UrlBasedCorsConfigurationSource();
+	    source.registerCorsConfiguration("/**", corsConfig);
+	 
+	    return new CorsWebFilter(source);
 	}
 
 }
